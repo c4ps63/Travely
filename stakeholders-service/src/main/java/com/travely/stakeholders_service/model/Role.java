b019024dd24eff3120f528071a187db0342f4ef6
c4ps63 <pavlekapuran1@gmail.com>
2026-06-13 00:46:18 +0100

    Add User model and Role enum

---
+++ stakeholders-service/src/main/java/com/travely/stakeholders_service/model/Role.java
@@ -0,0 +1,7 @@
+package com.travely.stakeholders_service.model;
+
+public enum Role {
+    TOURIST,
+    GUIDE,
+    ADMIN
+}
---
+++ stakeholders-service/src/main/java/com/travely/stakeholders_service/model/User.java
@@ -0,0 +1,39 @@
+package com.travely.stakeholders_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private String name;
+    private String surname;
+    private String profileImage;
+    private String biography;
+    private String motto;
+}
